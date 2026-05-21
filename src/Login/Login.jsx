2db0c7c17b51a3fa13d5c2f13c430648ed199d2b
rupeshbodineni <rupeshbodineni@gmail.com>
2026-05-21 14:45:55 +0530

    six

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -10,47 +10,33 @@ const Login = () => {
       className='card mx-auto mt-5 shadow p-4'
       style={{ width: '30rem' }}
     >
-
       <div className='card-header bg-white border-0'>
-
         <h1 className='card-title text-center fw-bold'>
           Sign In
         </h1>
-
         <p>
           Enter your mobile number and password to get started
         </p>
-
         <hr />
-
       </div>
-
       <div className='card-body bg-light'>
-
         <form>
-
           <div className="mb-3 text-start">
 
             <label className="form-label">
               Mobile
             </label>
-
             <div className="input-group">
-
               <span className="input-group-text">
                 +91
               </span>
-
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter mobile number"
               />
-
             </div>
-
           </div>
-
           <div className="mb-3 text-start">
 
             <label className="form-label">
@@ -59,9 +45,6 @@ const Login = () => {
              <div class="input-group">
                 {/* Lock Icon */}
       <span className='input-group-text'><i className="bi bi-lock"></i></span>
-
-
- 
               <input
               
                 type={showPassword ? "text" : "password"}
@@ -81,21 +64,14 @@ const Login = () => {
                 }
                   
               </span>
-
             </div>
-
-            {/* Forgot Password */}
             <div className='text-end mt-2'>
 
               <a href="#"  className="text-decoration-none text-dark">
                 Forgot Password?
               </a>
-
             </div>
-
           </div>
-
-          {/* Checkbox */}
           <div className='form-check mb-3'>
 
             <input
@@ -111,34 +87,22 @@ const Login = () => {
               I have read and accept the terms and conditions,
               privacy policy, and EULA agreement
             </label>
-
           </div>
-
-          {/* Button */}
           <div className='mb-3'>
-
             <button
               type='submit'
               className='btn btn-primary w-100  bg-white text-dark'
             >
               Continue
             </button>
-
           </div>
-
-          {/* Need Help */}
           <div className='text-center'>
-
             <a href="#"  className="text-decoration-none text-dark">
               Need help?
             </a>
-
           </div>
-
         </form>
-
       </div>
-
     </div>
 
   )
